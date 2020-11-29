e660ccbf77652f6bf5fe909cd591f630879c50b4
emmavdc <etu40268@henallux.be>
2020-11-29 20:36:21 +0100

    use axios to call api

---
+++ pets-services-admin/src/API/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL_API = "http://localhost:5000";
+
+const loginModel = {
+  password: String,
+  email: String,
+};
+
+const login = async (loginModel) => {
+
+    const rep = await axios.post(BASE_URL_API + "/users/actions/login", loginModel);
+    return rep.data;
+  
+};
+
+export { login, loginModel };
--- pets-services-admin/src/component/signin/index.js
+++ pets-services-admin/src/component/signin/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./signin.css";
 import logo from "../../assets/logo.svg";
 import { Redirect } from "react-router-dom";
+import { login, loginModel } from "../../API";
 
 class Signin extends React.Component {
-
   constructor() {
     super();
-    this.state = { email: "", password: "", errorMessage: "", redirectAfterLogin: false };
+    this.state = {
+      email: "",
+      password: "",
+      errorMessage: "",
+      redirectAfterLogin: false,
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -15,42 +20,46 @@ class Signin extends React.Component {
   handleChange(event) {
     let nam = event.target.name;
     let val = event.target.value;
-    this.setState({[nam]: val});
+    this.setState({ [nam]: val });
   }
   handleSubmit(event) {
+    loginModel.email = this.state.email;
+    loginModel.password = this.state.password;
 
-    const signInData = {
-      password: this.state.password,
-      email: this.state.email
-    };
+    const result = login(loginModel);
 
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(signInData)
-    };
-    fetch('http://localhost:5000/users/actions/login', requestOptions)
-        .then(response => response.text())
-        .then(data => {
-          sessionStorage.setItem("jwt", data);
-          this.setState({redirectAfterLogin: true});
-        });
+    result
+      .then((data) => {
+        sessionStorage.setItem("jwt", data);
+        this.setState({ redirectAfterLogin: true, errorMessage: "" });
+      })
+      .catch((e) => {
+        if (e.response.status === 401) {
+          this.setState({ errorMessage: "Utilisateur / mot de passe inconnu" });
+        } else {
+          this.setState({
+            errorMessage: "L'application est temporairement hors service",
+          });
+        }
+      });
+
     event.preventDefault();
   }
 
   render() {
-
     const { redirectAfterLogin } = this.state;
 
     if (redirectAfterLogin) {
-      return <Redirect to="/main/home"/>;
+      return <Redirect to="/main/home" />;
     }
 
     return (
       <div className="text-center">
         <img className="mb-4" src={logo} alt="" width="300" height="300"></img>
         <form className="form-signin" onSubmit={this.handleSubmit}>
-          <h1 className="h3 mb-3 font-weight-normal">Veuillez vous connecter</h1>
+          <h1 className="h3 mb-3 font-weight-normal">
+            Veuillez vous connecter
+          </h1>
           <label htmlFor="inputEmail" className="sr-only">
             Addresse mail
           </label>
@@ -76,6 +85,12 @@ class Signin extends React.Component {
             name="password"
             onChange={this.handleChange}
           ></input>
+          {this.state.errorMessage !== "" ? (
+            <div class="alert alert-warning" role="alert">
+              {this.state.errorMessage}
+            </div>
+          ) : null}
+
           <button className="btn btn-lg btn-primary btn-block" type="submit">
             Se connecter
           </button>
